ec654168400ead39624cf7c752eac5c58f77a90f
FrontRowWithJ <adebusum@tcd.ie>
2019-04-22 20:11:29 +0100

    1) Added the variable knownEquations 2) Improved the check function. 3) Added more functions

--- CheckButton.hpp
+++ CheckButton.hpp
@@ -51,30 +51,41 @@ class CheckButton
 	Color textColor = Color::White;
 	int numOfVisuals;
 	RectangleShape *visuals;
+	LinkedList *knownEquations;
 
   public:
 	CheckButton();
 	CheckButton(float width, float height, float xPos, float yPos, Font font);
-	void check(float mousePosX, float mousePosY, float screenPosX, float screenPosY, Board mabbleBoard); //this function needs to be more elaborated
+	void check(float mousePosX, float mousePosY, float screenPosX, float screenPosY, Board mabbleBoard, LinkedList *placedTiles); //this function needs to be more elaborated
 	void gen_visuals();
 	void gen_text();
 	void draw(RenderWindow *window);
 	static int compare_right(void *a, void *b);
 	static int compare_down(void *a, void *b);
 	static int compare_equation(void *a, void *b);
+	static char *to_string(void *a);
 
   private:
-	Direction is_same_line(LinkedList *placedTiles, BoardTile **table);
-	bool is_isolated(Board::TileData *td, BoardTile **table, int width);
-	bool are_tiles_contiguous(Direction d, BoardTile **table, LinkedList *placedTiles, int width);
-	LinkedList *gen_equation_list(BoardTile **table, int width, LinkedList *placedTiles, LinkedList *knownEquations);
-	Position get_equation_begin_pos(Direction d, BoardTile **table, int i, int j);
-	Equation *gen_equation(BoardTile **table, int width, Direction d, int i, int j);
-	void filter_equation_list(LinkedList *equationList);
+	Direction is_same_line(LinkedList *placedTiles, BoardTile ***table);
+	bool is_isolated(TileData *td, BoardTile ***table, int width);
+	bool are_tiles_contiguous(Direction d, BoardTile ***table, LinkedList *placedTiles, int width);
+	LinkedList *gen_equation_list(BoardTile ***table, int width, LinkedList *placedTiles);
+	Position get_equation_begin_pos(Direction d, BoardTile ***table, int i, int j);
+	Equation *gen_equation(BoardTile ***table, int width, Direction d, int i, int j);
+	void filter_equation_list(LinkedList *equationList); //!
 	string gen_incorrect_statement_string(LinkedList *equations);
-	void set_to_cant_remove(LinkedList *placedTiles, BoardTile **table);
-	evalResult_t *evaluate_equations(LinkedList *equations);
-	bool are_equations_valid(evalResult_t *resultCodes, int len);
-	LinkedList *split_list(LinkedList *equations, evalResult_t *resultCodes);
+	void set_to_cant_remove(LinkedList *placedTiles, BoardTile ***table);
+	evalResult_t *check_structure(LinkedList *equations, int *len);
+	bool are_equation_structure_valid(evalResult_t *resultCodes, int len);
+	evalResult_t *evaluate_equations(LinkedList *equations, long *totalScore, int *len);
+	bool are_equations_balanced(evalResult_t *resultCodes, int len);
+	LinkedList *split_list(LinkedList *equations, evalResult_t *resultCodes, evalResult_t code); //? I dont know full if this works with many equaions
+	void check_player(LinkedList *placedTiles, Board *board);		 //!
+	//? Checks the player's attempt
+	//? Should take a Player object as an argument.
+	//? Unsure of the return type
+	bool are_tiles_connected(LinkedList *placedTiles, BoardTile ***table, int width);
+	bool is_tile_positioned_correctly(TileData *td, BoardTile ***table, int width);
+	void add_to_known_equations(LinkedList *equations);
 };
 #endif
