24f84c353717df6dd2bdaca4093d4acd10567511
FrontRowWithJ <adebusum@tcd.ie>
2019-04-22 19:54:00 +0100

    1) Moved the struct "Direction" and "Equation" to another class 2) Added the struct TileData which holds the iPos, jPos and value of a tile placd ton the board. 3) Changed the table to a thriple pointer 4) Changed rowWidth to rowLen 5) Moved the function "check_players_turn" to a different class 6) Added the function "get_table" which returns the table 7) Added the function "get_width" which returns the width of the board 8) Added the function "get_rowLen" which returns the number of tiles in a row

--- Board.hpp
+++ Board.hpp
@@ -18,57 +18,42 @@
 #endif
 
 #define IS_EQUALS(V1, V2) \
-    V1.i == V2.i &&V1.j == V2.j
+  V1.i == V2.i &&V1.j == V2.j
 #define IS_BOARD_POS_EQUAL(X, Y) \
-    X.pos.i == Y.pos.i &&X.pos.j == Y.pos.j &&X.dir == Y.dir
+  X.pos.i == Y.pos.i &&X.pos.j == Y.pos.j &&X.dir == Y.dir
+typedef struct
+{
+  int i;
+  int j;
+  char value;
+} TileData;
 class Board
 {
-  public:
-    typedef enum
-    {
-        RIGHT,
-        DOWN,
-    } Direction;
+private:
+  BoardTile ***table;
+  float width;
+  int rowLen;
+  float xPos;
+  float yPos;
+  Tile nullTile = Tile();
+  Tile *selectedTile;
 
-    typedef struct
-    {
-        Position pos;
-        Direction dir;
-        string value;
-    } Equation;
-
-    typedef struct
-    {
-        int i;
-        int j;
-        char value;
-    } TileData;
-
-  private:
-    BoardTile **table;
-    float width;
-    int rowWidth;
-    float xPos;
-    float yPos;
-    Tile nullTile = Tile();
-    Tile *selectedTile;
-
-  public:
-    Board(float width, int rowWidth, float xPos, float yPos, Font font, Color textColor, Color bgColor);
-    bool place_tile(float mouseX, float mouseY, float screenX, float screenY, LinkedList *placedTiles);
-    bool remove_tile(float mouseX, float mouseY, float screenX, float screenY, LinkedList *placedTiles);
-    //* checkBoard
-    void check_players_turn(LinkedList *placedPieces, Equation *boardEquations, int len, evalResult_t *code);
-    void clear_board();
-    void draw(RenderWindow *window);
-    void set_selected_tile(Tile *selectedTile);
-    void clear_selected_tile(bool canClear);
-    static int compare(void *a, void *b);
-    static char *to_string(void *a);
-    void print_list(LinkedList *list);
-    BoardTile **get_table();
-
-  private:
-    void gen_board(Font font, Color textColor, Color bgColor);
+public:
+  Board(float width, int rowLen, float xPos, float yPos, Font font, Color textColor, Color bgColor);
+  bool place_tile(float mouseX, float mouseY, float screenX, float screenY, LinkedList *placedTiles);
+  bool remove_tile(float mouseX, float mouseY, float screenX, float screenY, LinkedList *placedTiles);
+  //* checkBoard
+  void clear_board();
+  void draw(RenderWindow *window);
+  void set_selected_tile(Tile *selectedTile);
+  void clear_selected_tile(bool canClear);
+  static int compare(void *a, void *b);
+  static char *to_string(void *a);
+  void print_list(LinkedList *list);
+  BoardTile ***get_table();
+  float get_width();
+  int get_rowLen();
+private:
+  void gen_board(Font font, Color textColor, Color bgColor);
 };
 #endif
