153698c6711036ce8f5e96b8d376f251da19dab9
FrontRowWithJ <adebusum@tcd.ie>
2019-05-04 20:55:15 +0100

    1) Added the function overload "delete node" which takes a Node pointer. 2) Added the function overload "find" which takes a Node pointer. 3) Function "clone" parameter list now accepts a function parameter which clones the value in a pointer. 4) Added the function "empty_list" which empties the list. 5) Added the function "pop" which removes the head of the list and returns it. 6) Added the function overload "get_parent" which takes a Node*.

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -29,22 +29,27 @@ public:
   void insert(void *val);
   bool contains(void *val, function<int(void *, void *)> compare);
   bool delete_node(void *val, function<int(void *, void *)> compare);
+  bool delete_node(Node *node);
   Node *remove(void *val, function<int(void *, void *)> compare);
   Node *find(void *val, function<int(void *, void *)> compare);
+  Node *find(Node *node);
   Node *get_parent(Node *node, function<int(void *, void *)> compare);
   int count();
   bool swap(void *val1, void *val2, function<int(void *, void *)> compare);
   bool llcat(LinkedList *list);
   LinkedList reverse_list();
-  LinkedList clone();
+  LinkedList clone(function<void *(void *)> clone_of);
   void print(function<char *(void *)> to_string);
   void sort(function<int(void *, void *)> compare);
+  void empty_list();
+  void *pop();
 
 private:
   int count_inner(Node *node);
   LinkedList reverse_inner(Node *node, LinkedList list);
   void insert(LinkedList list, void *val);
   bool swap(Node *node1, Node *node2);
+  Node *get_parent(Node *node);
 };
 
 #endif
