1840ebe148a6e3528158b825d7cae3c8df9c35f6
FrontRowWithJ <adebusum@tcd.ie>
2019-05-04 20:37:57 +0100

    1) Added operator overload "==" to Equation struct 2) Added turnLable which labels who's turn it is. 3) Function "CheckButton" parameter list now accepts coordinates for the turn label. 4) Function "check" parameter list accepts 2 player objects and a bool pointer that determines who's turn it is. 5) Function "gen_visuals" parameter list now accepts coordinatess for the turn label. 6) Function "gen_text" parameter list now accepts coordinates for the turn lable text label. 7) Added function "move_turnLable" which moves the the turn label. 8) Function "check_player" parameter list now accepts 2 player objects and a bool pointer which determines who's turn it is.

--- CheckButton.hpp
+++ CheckButton.hpp
@@ -4,6 +4,8 @@
 #include "Util.hpp"
 #include "Board.hpp"
 #include "Eval.hpp"
+#include "Player.hpp"
+#include "RoundedRectangle.hpp"
 
 class CheckButton
 {
@@ -23,12 +25,16 @@ class CheckButton
 		int j;
 	} Position;
 
-	typedef struct
+	typedef struct MathEquation
 	{
 		int startI;
 		int startJ;
 		Direction dir;
 		string value;
+		bool operator==(struct MathEquation e)
+		{
+			return startI == e.startI && startJ == e.startJ && dir == e.dir && (strcmp(value.c_str(), e.value.c_str()) == 0);
+		}
 	} Equation;
 
   private:
@@ -44,14 +50,17 @@ class CheckButton
 	int numOfVisuals;
 	RectangleShape *visuals;
 	LinkedList *knownEquations;
+	RoundedRectangle *turnLable;
+	Text turnText;
 
   public:
 	CheckButton();
-	CheckButton(float width, float height, float xPos, float yPos, Font font);
-	void check(float mousePosX, float mousePosY, float screenPosX, float screenPosY, Board mabbleBoard, LinkedList *placedTiles); //this function needs to be more elaborated
-	void gen_visuals();
-	void gen_text();
+	CheckButton(float width, float height, float xPos, float yPos, Font font, float turnXPos, float turnYPos);
+	void check(float mousePosX, float mousePosY, float screenPosX, float screenPosY, Board mabbleBoard, LinkedList *placedTiles, Player *playerOne, Player *playerTwo, bool *isPlayerOne); //this function needs to be more elaborated
+	void gen_visuals(float turnXPos, float turnYPos);
+	void gen_text(float turnXPos, float turnYPos);
 	void draw(RenderWindow *window);
+	void move_turnLable(float xPos, float yPos);
 	static int compare_right(void *a, void *b);
 	static int compare_down(void *a, void *b);
 	static int compare_equation(void *a, void *b);
@@ -71,8 +80,8 @@ class CheckButton
 	bool are_equation_structure_valid(evalResult_t *resultCodes, int len);
 	evalResult_t *evaluate_equations(LinkedList *equations, long *totalScore, int *len);
 	bool are_equations_balanced(evalResult_t *resultCodes, int len);
-	LinkedList *split_list(LinkedList *equations, evalResult_t *resultCodes, evalResult_t code); //? I dont know full if this works with many equaions
-	void check_player(LinkedList *placedTiles, Board *board);		 //!
+	LinkedList *split_list(LinkedList *equations, evalResult_t *resultCodes, evalResult_t code);				   //? I dont know full if this works with many equaions
+	void check_player(LinkedList *placedTiles, Board *board, Player *player1, Player *player2, bool *isPlayerOne); //!
 	//? Checks the player's attempt
 	//? Should take a Player object as an argument.
 	//? Unsure of the return type
