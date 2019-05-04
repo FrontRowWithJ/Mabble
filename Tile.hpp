f615c3bcc2f49bb094cd655c654d76345e847506
FrontRowWithJ <adebusum@tcd.ie>
2019-05-04 21:32:52 +0100

    1) Added the macro "IS_OPERAND" echic checks if the input is an operand (value between 0 and 9). 2) Added the constructor "Tile" which defines the behaviou of a null tile. 3) Removed the parameter "TileState type" from the constructor "Tile" parameter list. 4) Renamed the function "get_number_of_visuals" to "get_numOfVisuals". 5) Renamed the function "get_text_color" to "get_textColor". 6) Added the function "is_operator" which checks if is an operator. 7) Added getter functions for xPos, yPos, width and font.

--- Tile.hpp
+++ Tile.hpp
@@ -2,6 +2,8 @@
 #define TILE_H
 
 #include "Util.hpp"
+#define IS_OPERAND(X) \
+  (X - CHARACTER_OFFSET) >= 0 && (X - CHARACTER_OFFSET) < 10
 // This class defines the behavior of a Mabble Tile
 //? Each rack contains 10 board pieces (5 operands, 4 operators and 1 equal sign);
 //! Each piece will have functionality s.t when clicked upon their value will be saved as a global variable
@@ -27,28 +29,28 @@ class Tile
 {
   private:
     char value;
-    bool isSelected = false;
+    bool isSelected;
+    bool isOperator;
     TileState state;
     TileType type;
     //The visual parts
     RectangleShape *visuals;
     size_t numOfVisuals;
-    float xpos;
-    float ypos;
+    float xPos;
+    float yPos;
     float width;
     bool isNull;
-    // static int beige = 0xE6C194
     Color outLineColor = Color::Black;
     Font font;
     Text text;
-    int beige = 0xE6C194;
-    Color bgColor = Color(beige >> 16, (beige >> 8) & 0xFF, beige & 0xFF);
+    Color bgColor;
     //! This property is defined by the subclass:
     Color textColor; //depending on the type of value
   public:
     // The font object is passed to the user so as to avoid creating multiple uneccessary font objects
     Tile();
-    Tile(char value, float xpos, float ypos, float width, Font font, TileType type);
+    Tile(float xPos, float yPos, float width, Font font);
+    Tile(char value, float xPos, float yPos, float width, Font font);
     void gen_tile_visuals();
     void gen_text();
     Tile *select_tile();
@@ -56,18 +58,22 @@ class Tile
     void place_tile();
     Text get_text();
     RectangleShape *get_rectangles();
-    size_t get_number_of_visuals();
+    size_t get_numOfVisuals();
     bool is_selected();
     float get_xpos();
     float get_ypos();
-    float get_width();
     void draw(RenderWindow *window);
     bool is_null();
     char get_value();
     void set_state(TileState state);
     TileState get_state();
-    Color get_text_color();
+    Color get_textColor();
     bool operator!=(Tile tile);
-    void set_is_selected(bool isSelected);
+    void set_isSelected(bool isSelected);
+    bool is_operator();
+    float get_xPos();
+    float get_yPos();
+    float get_width();
+    Font get_font();
 };
 #endif
