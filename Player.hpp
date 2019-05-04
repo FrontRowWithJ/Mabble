099c4b41fbe7033eacce71c7d7bcbdd365b9cbda
FrontRowWithJ <adebusum@tcd.ie>
2019-05-04 21:05:54 +0100

    1) Added the subclass "ScoreBoard" which is the score board. 2) Added Player constructor. 3) Added Function "draw" which draws the player scoreboard and tilerack. 4) Added the function "update_tileRack" which updates the tilerack with new tiles. 5) Added the function "update_score" which updates the score board. 6) Added the fucntion "get_tile_rack" which returns the tile rack. 7) Added the function "switch_turn" which switches the turn of the player. 8) Added the function "deselect_tile" which deselects a tile from the tilerack. 9) Added getter functions for xPos, yPos, width and height.

--- Player.hpp
+++ Player.hpp
@@ -3,28 +3,84 @@
 
 #include "Util.hpp"
 #include "Board.hpp"
+#include "TileRack.hpp"
+#include "TileBag.hpp"
 
 #define PIECE_COUNT 10
-#define TOTAL_PIECES 100
+#define TOTAL_PIECES 40
+
+//properties of player:
+// ? indicates if this property isn't certain
+// ? boolean isTurn
+// ? number of turns
+// ? number of pieces left
+//current set of pieces (operands and operators)
+//score
+//successful equations
+
+//note: If the player's turn is verified to be successful then we need to make sure that we don't
+//give the user additional points for valid equations that were adjacent to the equation that the user has created
+// and already existed.
 
 class Player
 {
-  public:
-    typedef struct
-    {
-        bool isPlaced;
-        char value;
-    } Piece;
+private:
+	class ScoreBoard
+	{
+	private:
+		float xPos;
+		float yPos;
+		float width;
+		float height;
+		long score;
+		Text scoreText;
+		Font font;
+		RectangleShape *visuals;
+		Color bgColor;
+		Color textColor;
+		int numOfVisuals;
 
-  public:
-    bool isTurn;
-    int numOfTurns;
-    long score;
-    LinkedList placedPieces;
-    Piece currentPieces[PIECE_COUNT];
-    Piece pieceBag[TOTAL_PIECES];
+	public:
+		ScoreBoard(float xPos, float yPos, float width, float height, Font font);
+		void update_score_board(long score);
+		void draw(RenderWindow *window);
+		static char *ltoa(long num);
 
-  public:
-    LinkedList getPlacedPieces();
+	private:
+		void gen_visuals();
+		void gen_text();
+	};
+
+private:
+	float xPos;
+	float yPos;
+	float width;
+	float height;
+	float fgScale;
+	bool isTurn;
+	int numOfTurns;
+	long score;
+	TileBag *tileBag;
+	TileRack *tileRack;
+	string playerName;
+	Font font;
+	Text nameText;
+	Text scoreText;
+	ScoreBoard *sb;
+
+public:
+	Player();
+	Player(float xPos, float yPos, float width, float height, float fgScale, string playerName, Font font, bool isTurn);
+	void draw(RenderWindow *window);
+	void update_tileRack();
+	void update_score(long points);
+	TileRack *get_tile_rack();
+	void switch_turn();
+	Tile *select_tile(float mouseX, float mouseY, float screenX, float screenY);
+	bool deselect_tile(float mouseX, float mouseY, float screenX, float screenY);
+	float get_xPos();
+	float get_yPos();
+	float get_width();
+	float get_height();
 };
 #endif
