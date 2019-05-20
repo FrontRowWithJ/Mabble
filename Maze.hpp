0e548fa9f218b3a0c3277ba7895c8520d8a297d6
FrontRowWithJ <adebusum@tcd.ie>
2019-05-20 13:23:29 +0100

    1) Added variables "screenWidth, screenHeight, panSpeed, threshold, startPos, displayPos and valPos". 2) In constructor "Maze" added the variables screenWidth and screenHeight to the parameter list. 4) Added the function "update_tile_corners". 5) Functin "gen_row" is now private

--- Maze.hpp
+++ Maze.hpp
@@ -9,21 +9,28 @@ class Maze
 {
 private:
     size_t columnLen;
+    size_t screenWidth;
+    size_t screenHeight;
     LinkedList *columns;
     LinkedList *displayColumn;
     int xPos;
     int yPos;
     int width;
-
+    float panSpeed;
+    float threshold;
+    float startPos;
+    Node * displayPos;
+    Node *valPos;
 public:
-    Maze(size_t columnLen);
+    Maze(size_t columnLen, size_t screenWidth, size_t screenHeight);
     void display_matrix(RenderWindow *window);
-    void gen_row();
     bool **to_matrix(size_t *width, size_t *height);
     int get_width();
     int get_height();
 
 private:
     void update_display();
+    void gen_row();
+    void update_tile_corners();
 };
 #endif
