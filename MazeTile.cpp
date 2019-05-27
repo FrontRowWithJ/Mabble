b4b730878c9375368fde54353d03f1f95514b4e7
FrontRowWithJ <adebusum@tcd.ie>
2019-05-27 11:52:40 +0100

    1) The font for the text is no longer loaded locally. 2) THe text is no longer generated locally.

--- MazeTile.cpp
+++ MazeTile.cpp
@@ -6,28 +6,24 @@ MazeTile::MazeTile()
 
 MazeTile::MazeTile(float xPos, float yPos, float width, float radius, Font font, char value, bool isTextPresent)
 {
-    // isTextPresent = false;
     bg = new RoundedRectangle(xPos, yPos, width, width, radius, 7);
     this->value = value;
     this->font = font;
-    this->font.loadFromFile("run.ttf");
-    this->operandColor = COLOR(0x270F36);
-    this->operatorColor = COLOR(0x632B6C);
-    this->equalColor = COLOR(0xC76B98);
-    this->bgColor = COLOR(0xFCC3A3);
+    int alpha = static_cast<int>(255 * 0.5);
+    this->operandColor = COLOR(0x270F36, alpha);
+    this->operatorColor = COLOR(0x632B6C, alpha);
+    this->equalColor = COLOR(0xC76B98, alpha);
+    this->bgColor = isTextPresent ? COLOR(0xC76B98, alpha) : COLOR(0xFFFFFF, alpha);
+    bg->set_thickness(0.0f);
     bg->set_fill_color(bgColor);
     bg->set_outline_color(bgColor);
     this->isTextPresent = isTextPresent;
-    if (isTextPresent)
-        gen_text();
 }
 
 void MazeTile::gen_text()
 {
-    text.setFont(font);
-    text.setCharacterSize(bg->get_width());
-    text.setString(new char[2]{value, '\0'});
     float xPos = bg->get_xPos(), yPos = bg->get_yPos(), width = bg->get_width();
+    text = Text(new char[2]{value, '\0'}, font, width);
     FloatRect lb = text.getLocalBounds();
     text.setOrigin(SET_XPOS(xPos, width), SET_YPOS(yPos, width));
     if (value >= '0' && value <= '9')
