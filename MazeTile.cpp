325012fd37b6795b38734ecb25ac958c965a99f9
FrontRowWithJ <adebusum@tcd.ie>
2019-05-28 18:31:33 +0100

    1) The RoundedRectangle is now longer stored to a pointer.

--- MazeTile.cpp
+++ MazeTile.cpp
@@ -6,7 +6,7 @@ MazeTile::MazeTile()
 
 MazeTile::MazeTile(float xPos, float yPos, float width, float radius, Font font, char value, bool isTextPresent)
 {
-    bg = new RoundedRectangle(xPos, yPos, width, width, radius, 7);
+    bg = RoundedRectangle(xPos, yPos, width, width, radius, 7);
     this->value = value;
     this->font = font;
     int alpha = static_cast<int>(255 * 0.5);
@@ -14,15 +14,15 @@ MazeTile::MazeTile(float xPos, float yPos, float width, float radius, Font font,
     this->operatorColor = COLOR(0x632B6C, alpha);
     this->equalColor = COLOR(0xC76B98, alpha);
     this->bgColor = isTextPresent ? COLOR(0xC76B98, alpha) : COLOR(0xFFFFFF, alpha);
-    bg->set_thickness(0.0f);
-    bg->set_fill_color(bgColor);
-    bg->set_outline_color(bgColor);
+    bg.set_thickness(0.0f);
+    bg.set_fill_color(bgColor);
+    bg.set_outline_color(bgColor);
     this->isTextPresent = isTextPresent;
 }
 
 void MazeTile::gen_text()
 {
-    float xPos = bg->get_xPos(), yPos = bg->get_yPos(), width = bg->get_width();
+    float xPos = bg.get_xPos(), yPos = bg.get_yPos(), width = bg.get_width();
     text = Text(new char[2]{value, '\0'}, font, width);
     FloatRect lb = text.getLocalBounds();
     text.setOrigin(SET_XPOS(xPos, width), SET_YPOS(yPos, width));
@@ -38,75 +38,70 @@ void MazeTile::gen_text()
 
 void MazeTile::draw(RenderWindow *window)
 {
-    bg->draw(window);
+    bg.draw(window);
     if (isTextPresent)
         window->draw(text);
 }
 
 void MazeTile::set_position(float xPos, float yPos)
 {
-    bg->set_position(xPos, yPos);
+    bg.set_position(xPos, yPos);
     if (isTextPresent)
     {
         FloatRect lb = text.getLocalBounds();
-        float width = bg->get_width();
+        float width = bg.get_width();
         text.setOrigin(SET_XPOS(xPos, width), SET_YPOS(yPos, width));
     }
 }
 
 float MazeTile::get_xPos()
 {
-    return bg->get_xPos();
+    return bg.get_xPos();
 }
 
 float MazeTile::get_yPos()
 {
-    return bg->get_yPos();
+    return bg.get_yPos();
 }
 
 void MazeTile::set_fill_color(Color bgColor)
 {
-    bg->set_fill_color(bgColor);
+    bg.set_fill_color(bgColor);
 }
 
 void MazeTile::set_outline_color(Color outlineColor)
 {
-    bg->set_outline_color(outlineColor);
+    bg.set_outline_color(outlineColor);
 }
 
-void MazeTile::set_radiusA(float radiusA)
+void MazeTile::set_radiusA()
 {
-    bg->set_radiusA(radiusA);
+    bg.set_radiusA_to_zero();
 }
 
-void MazeTile::set_radiusB(float radiusB)
+void MazeTile::set_radiusB()
 {
-    bg->set_radiusB(radiusB);
+    bg.set_radiusB_to_zero();
 }
 
-void MazeTile::set_radiusC(float radiusC)
+void MazeTile::set_radiusC()
 {
-    bg->set_radiusC(radiusC);
+    bg.set_radiusC_to_zero();
 }
 
-void MazeTile::set_radiusD(float radiusD)
+void MazeTile::set_radiusD()
 {
-    bg->set_radiusD(radiusD);
-}
-
-void MazeTile::del()
-{
-    delete bg;
+    bg.set_radiusD_to_zero();
 }
 
 void MazeTile::set_xPos(float xPos)
 {
-    float yPos = bg->get_yPos();
+    float yPos = bg.get_yPos();
     set_position(xPos, yPos);
 }
 
 void MazeTile::set_yPos(float yPos)
 {
-    float xPos = bg->get_xPos();
+    float xPos = bg.get_xPos();
     set_position(xPos, yPos);
 }
