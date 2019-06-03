8ae23adb2dfb440c5b0c8241ac127af488f3c79a
FrontRowWithJ <adebusum@tcd.ie>
2019-06-03 21:37:33 +0100

    1) Changed Test.cpp Main.cpp.

--- Mabble.sh
+++ Mabble.sh
@@ -2,5 +2,5 @@
 
 #Run the game
 
-g++ Test.cpp Eval.cpp RoundedRectangle.cpp TileBag.cpp Player.cpp Board.cpp BoardTile.cpp CheckButton.cpp Tile.cpp TileRack.cpp LinkedList.cpp -lsfml-graphics -lsfml-window -lsfml-system -o Test;
+g++ Main.cpp Eval.cpp RoundedRectangle.cpp TileBag.cpp Player.cpp Board.cpp BoardTile.cpp CheckButton.cpp Tile.cpp TileRack.cpp LinkedList.cpp -lsfml-graphics -lsfml-window -lsfml-system -o Test;
 ./Test
